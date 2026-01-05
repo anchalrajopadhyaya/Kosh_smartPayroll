d404d982142a012fa699a3b2e006c1e9439ab544
anchalrajopadhyaya <anchalrajopadhyaya@gmail.com>
2026-01-05 14:28:23 +0545

    UIfeat: add employee detail

--- lib/frontend/empDetails.dart
+++ lib/frontend/empDetails.dart
@@ -1,18 +1,29 @@
-// employee_screen.dart
 import 'package:flutter/material.dart';
+import 'empAdd.dart';
 
 class EmployeeScreen extends StatelessWidget {
   const EmployeeScreen({super.key});
+
+  static const Color primaryColor = Color.fromARGB(255, 24, 137, 132);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Employees')),
+      appBar: AppBar(
+        title: const Text('Employees'),
+        backgroundColor: primaryColor,
+        foregroundColor: Colors.white,
+      ),
+
       body: const Center(child: Text('Employee list will go here')),
+
       floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: primaryColor,
+        foregroundColor: Colors.white,
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (_) => const EmployeeScreen()),
+            MaterialPageRoute(builder: (_) => const AddEmployeeScreen()),
           );
         },
         icon: const Icon(Icons.person_add),
